37633fe0ce9372bf41527009bfd316027ed69d99
Alexander <Alexander_alatorre1993@hotmail.com>
2022-10-06 20:53:38 -0500

    reset logic to an MVC architecture

--- express/server.js
+++ express/server.js
@@ -1,73 +1,38 @@
 const express = require("express");
+const path = require("path");
+
+const friendsRouter = require("./routes/friends.router");
+const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
-const PORT = process.env.PORT || 8081;
+// views
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
-const friends = [
-  {
-    id: 1,
-    name: "alex",
-  },
-  {
-    id: 2,
-    name: "josh",
-  },
-];
-
-//middleware
+// middleware
 app.use((req, res, next) => {
   const start = Date.now();
-
-  // without next, the response hangs
   next();
-
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
-
-app.get("/friends", (req, res) => {
-  res.json(friends);
-});
-
-app.get("/friends/:friendId", (req, res) => {
-  const friendId = +req.params.friendId;
-  const friend = friends[friendId];
-
-  if (!friend) {
-    return res.status(404).json({
-      error: "Friend does not exist",
-    });
-  }
-
-  return res.status(200).json(friend);
-});
-
-app.get("/messages", (req, res) => {
-  res.send("<ul><li>Hello Alex</li></ul>");
-});
-
-//middleware for json parsing
+app.use("/site", express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.post("/friends", (req, res) => {
-  if (!req.body.name) {
-    return res.status(400).json({
-      error: "Missing friend name",
-    });
-  }
 
-  const newFriend = {
-    name: req.body.name,
-    id: friends.length,
-  };
-
-  friends.push(newFriend);
-
-  res.json(newFriend);
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "My Friends Are VERYY Clever",
+    caption: "Let's go skiing!",
+  });
 });
 
+// routes
+app.use("/friends", friendsRouter);
+app.use("/messages", messagesRouter);
+
+// listening on port
+const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`running on port ${PORT}`);
+  console.log(`Listening on ${PORT}...`);
 });
-
-app.use(function (req, res, next) {});
